206395ee377f6664dc99e85f99c794d40d1a3361
Daniel Ulch <dn549348@dal.ca>
2016-08-15 13:22:19 -0300

    Release Build

--- RFEM Software/RFEM Software/obj/Release/RFEMDesktopClient_Content.g.i.cs
+++ RFEM Software/RFEM Software/obj/Release/RFEMDesktopClient_Content.g.i.cs
@@ -8,7 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("dj.mp3")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("dj_.wav")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("rfemsoftware.pfx")]
 
 
